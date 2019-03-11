ad2f9d55a6b7b330665bcfb225a92ab516269b2d
Andrei Chiperi <44604723+chiperiandrei@users.noreply.github.com>
2019-03-11 09:56:24 +0300

    Add files via upload

---
+++ java - uri/Film.java
@@ -0,0 +1,21 @@
+// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   Film.java
+
+
+public class Film extends GalerieMultimedia
+{
+
+    public Film()
+    {
+    }
+
+    public void afisareFilm()
+    {
+    }
+
+    public void gasireFilm()
+    {
+    }
+}
