4eb1a41417bb7adc33c928787c612057d1a97547
Andrei Chiperi <44604723+chiperiandrei@users.noreply.github.com>
2019-03-11 10:27:39 +0300

    Add files via upload

---
+++ Reverse/java - uri/Exponat.java
@@ -0,0 +1,36 @@
+// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   Exponat.java
+
+import java.time.LocalTime;
+import java.util.Vector;
+
+public abstract class Exponat
+{
+
+    public Exponat()
+    {
+    }
+
+    public void alegeAutor()
+    {
+    }
+
+    public void preiaGalerieImagini()
+    {
+    }
+
+    public void afisareExponat()
+    {
+    }
+
+    public String denumire;
+    public String autor;
+    public LocalTime dataRealizare;
+    public Integer dimensiune;
+    public Vector myVizitator;
+    public Vector Vizualizare;
+    public GalerieMultimedia myGalerieMultimedia;
+    public Vector PanelAdministrare;
+}
